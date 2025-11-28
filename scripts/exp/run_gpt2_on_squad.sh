07c7bb2dfaf1996a146cb65497087b395729abd2
booydar <ayd98@mail.ru>
2025-11-28 11:56:57 +0000

    add squad scripts for rmt

--- scripts/exp/run_gpt2_on_squad.sh
+++ scripts/exp/run_gpt2_on_squad.sh
@@ -16,26 +16,15 @@ PRETRAINED_MODEL=gpt2
 N_MEM_TOKENS=8
 N_CTRL_TOKENS=0
 K=2
-INNER_LR=0.25
-INNER_CLIP_VALUE=None
-INNER_CLIP_NORM=None
-USE_ADAM=false
-GRAD_MODE="second"
 USE_MEM_PROJ=true
 MEM_PROJ_MODE="proj"
 USE_WRITE_HEAD=true
 
-RUN_NAME=gradmem_${MODEL_NAME}_mem${N_MEM_TOKENS}
+RUN_NAME=${MODEL_NAME}_mem${N_MEM_TOKENS}
 if [ "$N_CTRL_TOKENS" -gt 0 ]; then
   RUN_NAME=${RUN_NAME}_c${N_CTRL_TOKENS}
 fi
-RUN_NAME=${RUN_NAME}_K${K}_ilr${INNER_LR}
-if [ "$INNER_CLIP_VALUE" != "None" ]; then
-  RUN_NAME=${RUN_NAME}_icv${INNER_CLIP_VALUE}
-fi
-if [ "$INNER_CLIP_NORM" != "None" ]; then
-  RUN_NAME=${RUN_NAME}_icn${INNER_CLIP_NORM}
-fi
+RUN_NAME=${RUN_NAME}_K${K}
 if [ "$USE_MEM_PROJ" = true ]; then
   RUN_NAME=${RUN_NAME}_mem_proj
   if [ "$MEM_PROJ_MODE" == "per_sample" ]; then
@@ -44,13 +33,6 @@ if [ "$USE_MEM_PROJ" = true ]; then
 fi
 if [ "$USE_WRITE_HEAD" = true ]; then
   RUN_NAME=${RUN_NAME}_whead
-fi
-RUN_NAME=${RUN_NAME}_grad_${GRAD_MODE}
-if [ "$USE_ADAM" = true ]; then
-  RUN_NAME=${RUN_NAME}_with_adam
-fi
-if [ "$USE_GRAD_CKPT" = true ]; then
-  RUN_NAME=${RUN_NAME}_gc
 fi
 RUN_NAME=${RUN_NAME}_bs_${TBS}_lr_${LR}
 
@@ -70,7 +52,7 @@ for N in "${N_VALUES[@]}"; do
     --num_processes $NP \
     --mixed_precision bf16 \
     --config_file accelerate.yaml \
-    run_gradmemgpt_on_squad.py \
+    run_gpt2_on_squad.py \
     --exp_path $EXP_PATH \
     --per_device_batch_size $PER_DEVICE_BATCH_SIZE \
     --gradient_accumulation_steps $GRAD_ACC_STEPS \
@@ -78,17 +60,6 @@ for N in "${N_VALUES[@]}"; do
     --data_path $DATA_PATH \
     --learning_rate $LR \
     --pretrained_model $PRETRAINED_MODEL \
-    --n_mem_tokens $N_MEM_TOKENS \
-    --K $K \
-    --inner_lr $INNER_LR \
-    --use_adam $USE_ADAM \
-    --grad_mode $GRAD_MODE \
-    $( [ "$INNER_CLIP_VALUE" != "None" ] && echo "--inner_clip_value $INNER_CLIP_VALUE" ) \
-    $( [ "$INNER_CLIP_NORM" != "None" ] && echo "--inner_clip_norm $INNER_CLIP_NORM" ) \
-    $( [ "$USE_MEM_PROJ" = true ] && echo "--use_mem_proj" ) \
-    $( [ "$USE_MEM_PROJ" = true ] && echo "--mem_proj_mode $MEM_PROJ_MODE" ) \
-    $( [ "$USE_WRITE_HEAD" = true ] && echo "--use_write_head" ) \
-    $( [ "$USE_GRAD_CKPT" = true ] && echo "--use_gradient_checkpointing" ) \
     --max_steps 200000 \
     --eval_steps 500 \
     --logging_steps 500 \
