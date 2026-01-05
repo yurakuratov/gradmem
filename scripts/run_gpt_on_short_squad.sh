3479375dfa5ce6208cd3572b246a9cb9557c001b
mkairov <kairovmatvey@gmail.com>
2026-01-05 15:51:57 +0300

    feat: add support for shortened SQuAD

---
+++ scripts/run_gpt_on_short_squad.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+set -e
+export TOKENIZERS_PARALLELISM=false
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export CUBLAS_WORKSPACE_CONFIG=:4096:2
+export CUDA_LAUNCH_BLOCKING=1
+export CUDA_VISIBLE_DEVICES=1,3
+NP=2
+
+# Define arguments for the script
+LR=1e-04
+TBS=64
+PER_DEVICE_BATCH_SIZE=32
+GRAD_ACC_STEPS=$(($TBS/($PER_DEVICE_BATCH_SIZE*$NP)))
+
+L=4
+H=4
+D=128
+# MAX_POSITION_EMBEDDINGS=1024
+BASE_MODEL=gpt2
+
+V=62
+# Dataset parameters
+# DATA_NAME="N2-K4V4-S4(32-64)_1M"
+# DATA_NAME="N1-K4V4-S1(16-32)_1M"
+# DATA_NAME="N10-K2V2-S4(32-64)_1M"
+# DATA_NAME="N8-K1V1-vocab512_1M"
+# DATA_NAME="N64-K2V2-V${V}_1M"
+# DATA_NAME="N4-K1V1-vocab512_1M"
+# copy task
+# DATA_NAME="N0-S1(4-4)_1M"
+DATA_NAME="squad_short"
+DATA_PATH="./data/${DATA_NAME}"
+TOKENIZER_PATH="gpt2"
+
+if [ "$BASE_MODEL" == "mamba" ]; then
+  RUN_NAME="${BASE_MODEL}_L${L}D${D}"
+else
+  RUN_NAME="${BASE_MODEL}_L${L}H${H}D${D}"
+  if [ -n "$MAX_POSITION_EMBEDDINGS" ]; then
+    RUN_NAME="${RUN_NAME}_L${MAX_POSITION_EMBEDDINGS}"
+  fi
+fi
+
+RUN_NAME=${RUN_NAME}_bs_${TBS}_lr_${LR}
+
+if [ -n "$ADAM_BETA2" ]; then
+  RUN_NAME=${RUN_NAME}_b2_${ADAM_BETA2}
+fi
+
+
+# Run ID
+N_VALUES=(2)
+for N in "${N_VALUES[@]}"; do
+  # Path to save experiment results
+  EXP_PATH="/cephfs/home/mkairov/gd_runs/${DATA_NAME}/${RUN_NAME}/run_$N"
+
+  # Execute the script using accelerate for parallel processing
+  accelerate launch \
+    --main_process_port $((29500+$TBS+$N+1)) \
+    --num_processes $NP \
+    --mixed_precision bf16 \
+    --config_file accelerate.yaml \
+    run_gpt2_on_short_squad.py \
+    --exp_path $EXP_PATH \
+    --per_device_batch_size $PER_DEVICE_BATCH_SIZE \
+    --gradient_accumulation_steps $GRAD_ACC_STEPS \
+    --total_batch_size $TBS \
+    --data_path $DATA_PATH \
+    --tokenizer_path $TOKENIZER_PATH \
+    --learning_rate $LR \
+    $( [ -n "$ADAM_BETA2" ] && echo "--adam_beta2 $ADAM_BETA2" ) \
+    --n_layer $L \
+    --n_head $H \
+    --n_embd $D \
+    $( [ -n "$MAX_POSITION_EMBEDDINGS" ] && echo "--max_position_embeddings $MAX_POSITION_EMBEDDINGS" ) \
+    --pretrained_model $BASE_MODEL \
+    --max_steps 200000 \
+    --eval_steps 500 \
+    --logging_steps 500 \
+    --warmup_steps 10000 \
+    --early_stopping_patience 500 \
+    --seed $((142+$N))
+done
+
+echo "Done"
---
+++ scripts/run_gradmemgpt_on_short_squad.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+set -e
+export TOKENIZERS_PARALLELISM=false
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export CUBLAS_WORKSPACE_CONFIG=:4096:2
+export CUDA_LAUNCH_BLOCKING=1
+export CUDA_VISIBLE_DEVICES=0,1
+NP=2
+
+LR=1e-04
+TBS=64
+PER_DEVICE_BATCH_SIZE=32
+GRAD_ACC_STEPS=$(($TBS/($PER_DEVICE_BATCH_SIZE*$NP)))
+USE_GRAD_CKPT=false
+
+MODEL_NAME=gpt2
+PRETRAINED_MODEL=gpt2
+
+
+# GradMemGPT specific parameters
+N_MEM_TOKENS=8
+N_CTRL_TOKENS=0
+K=2
+INNER_LR=0.4
+INNER_CLIP_VALUE=None
+INNER_CLIP_NORM=None
+USE_ADAM=false
+GRAD_MODE="second"
+USE_MEM_PROJ=false
+MEM_PROJ_MODE="none"
+USE_WRITE_HEAD=true
+
+RUN_NAME=gradmem_${MODEL_NAME}_mem${N_MEM_TOKENS}
+if [ "$N_CTRL_TOKENS" -gt 0 ]; then
+  RUN_NAME=${RUN_NAME}_c${N_CTRL_TOKENS}
+fi
+RUN_NAME=${RUN_NAME}_K${K}_ilr${INNER_LR}
+if [ "$INNER_CLIP_VALUE" != "None" ]; then
+  RUN_NAME=${RUN_NAME}_icv${INNER_CLIP_VALUE}
+fi
+if [ "$INNER_CLIP_NORM" != "None" ]; then
+  RUN_NAME=${RUN_NAME}_icn${INNER_CLIP_NORM}
+fi
+if [ "$USE_MEM_PROJ" = true ]; then
+  RUN_NAME=${RUN_NAME}_mem_proj
+  if [ "$MEM_PROJ_MODE" == "per_sample" ]; then
+    RUN_NAME=${RUN_NAME}_ps
+  fi
+fi
+if [ "$USE_WRITE_HEAD" = true ]; then
+  RUN_NAME=${RUN_NAME}_whead
+fi
+RUN_NAME=${RUN_NAME}_grad_${GRAD_MODE}
+if [ "$USE_ADAM" = true ]; then
+  RUN_NAME=${RUN_NAME}_with_adam
+fi
+if [ "$USE_GRAD_CKPT" = true ]; then
+  RUN_NAME=${RUN_NAME}_gc
+fi
+RUN_NAME=${RUN_NAME}_bs_${TBS}_lr_${LR}_init_gpt2
+
+DATA_NAME="squad_short"
+DATA_PATH="./data/${DATA_NAME}"
+TOKENIZER_PATH="gpt2"
+
+# Run ID
+N_VALUES=(1)
+for N in "${N_VALUES[@]}"; do
+  RND=$(date +%Y%m%d%H%M%S)
+  # Path to save experiment results
+  EXP_PATH="/cephfs/home/mkairov/gd_runs/${DATA_NAME}/${RUN_NAME}/run_$N"
+
+  # Execute the script using accelerate for parallel processing
+  accelerate launch \
+    --main_process_port $((29500+$TBS+$N+1)) \
+    --num_processes $NP \
+    --mixed_precision bf16 \
+    --config_file accelerate.yaml \
+    run_gradmemgpt_on_short_squad.py \
+    --exp_path $EXP_PATH \
+    --per_device_batch_size $PER_DEVICE_BATCH_SIZE \
+    --gradient_accumulation_steps $GRAD_ACC_STEPS \
+    --total_batch_size $TBS \
+    --data_path $DATA_PATH \
+    --tokenizer_path $TOKENIZER_PATH \
+    --learning_rate $LR \
+    --n_mem_tokens $N_MEM_TOKENS \
+    --K $K \
+    --inner_lr $INNER_LR \
+    --use_adam $USE_ADAM \
+    --grad_mode $GRAD_MODE \
+    $( [ "$INNER_CLIP_VALUE" != "None" ] && echo "--inner_clip_value $INNER_CLIP_VALUE" ) \
+    $( [ "$INNER_CLIP_NORM" != "None" ] && echo "--inner_clip_norm $INNER_CLIP_NORM" ) \
+    $( [ "$USE_MEM_PROJ" = true ] && echo "--use_mem_proj" ) \
+    $( [ "$USE_MEM_PROJ" = true ] && echo "--mem_proj_mode $MEM_PROJ_MODE" ) \
+    $( [ "$USE_WRITE_HEAD" = true ] && echo "--use_write_head" ) \
+    $( [ "$USE_GRAD_CKPT" = true ] && echo "--use_gradient_checkpointing" ) \
+    --max_steps 200000 \
+    --eval_steps 500 \
+    --logging_steps 500 \
+    --warmup_steps 10000 \
+    --early_stopping_patience 500 \
+    --seed $((142+$N)) \
+    --base_model $MODEL_NAME
+    # --init_checkpoint "model.safetensors"
+done
+
+echo "Done"
